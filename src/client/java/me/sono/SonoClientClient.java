2faa40a0d0477457dc327d2392561f227f73f48b
Sono <115237794+getSono@users.noreply.github.com>
2024-04-15 19:12:38 +0200

    Add main

---
+++ src/client/java/me/sono/SonoClientClient.java
@@ -0,0 +1,10 @@
+package me.sono;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SonoClientClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
